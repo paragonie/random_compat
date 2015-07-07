62481cda93ca55230a44fc5d03d560a4f80170d4
Scott <scott@paragonie.com>
2015-07-07 19:13:51 -0400

    Add some tests on the distribution of random values.

--- tests/unit/RandomIntTest.php
+++ tests/unit/RandomIntTest.php
@@ -17,4 +17,16 @@ class RandomIntTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($integers[0] <= 1000 && $integers[0] >= 0);
         $this->assertTrue($integers[1] < 2001 && $integers[1] > 1000);
     }
+    
+    public function testDistribution()
+    {
+        $integers = array_fill(0, 100, 0);
+        for ($i = 0; $i < 10000; ++$i) {
+            ++$integers[random_int(0,99)];
+        }
+        for ($i = 0; $i < 100; ++$i) {
+            $this->assertFalse($integers[$i] < 30);
+            $this->assertFalse($integers[$i] > 170);
+        }
+    }
 }
