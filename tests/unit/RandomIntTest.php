35359ae3451e7ecfbac4c5bf2afa1a83ba457a2d
Scott <scott@paragonie.com>
2015-07-08 05:23:42 -0400

    Add unit tests for negative integers.

--- tests/unit/RandomIntTest.php
+++ tests/unit/RandomIntTest.php
@@ -10,11 +10,15 @@ class RandomIntTest extends PHPUnit_Framework_TestCase
     {
         $integers = [
             random_int(0, 1000),
-            random_int(1001,2000)
+            random_int(1001,2000),
+            random_int(-100, -10),
+            random_int(-1000, 1000)
         ];
         
         $this->assertFalse($integers[0] === $integers[1]);
-        $this->assertTrue($integers[0] <= 1000 && $integers[0] >= 0);
-        $this->assertTrue($integers[1] < 2001 && $integers[1] > 1000);
+        $this->assertTrue($integers[0] >= 0 && $integers[0] <= 1000);
+        $this->assertTrue($integers[1] >= 1001 && $integers[1] <= 2000);
+        $this->assertTrue($integers[2] >= -100 && $integers[2] <= -10);
+        $this->assertTrue($integers[2] >= -1000 && $integers[2] <= 1000);
     }
 }
