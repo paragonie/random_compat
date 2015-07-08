58a4993595db71a801a4d242c98cd82a7e7fad20
Scott <scott@paragonie.com>
2015-07-07 23:11:29 -0400

    Make tolerance calculations easier to read/configure, less error-prone

--- tests/full/DieHardTest.php
+++ tests/full/DieHardTest.php
@@ -10,16 +10,21 @@ class DieHardTest extends PHPUnit_Framework_TestCase
     {
         // Number of buckets to make
         $num_buckets = 17;
+        // How much tolerance should we allow? 0.01 = 1% 0.50 = 50%, etc.
+        $tolerance = 0.03;
         $rand_min = 200000;
         $rand_max = 600000;
         $rand_step = 100000;
+        
+        $minT = (1.00 - $tolerance);
+        $maxT = (1.00 + $tolerance);
         
         for ($nums_to_generate = $rand_min; $nums_to_generate < $rand_max; $nums_to_generate += $rand_step) {
             $buckets = array_fill(0, $num_buckets, 0);
 
             // The number of ints we expect per bucket +/- 2%;
-            $min = (int) ceil(0.97 * $nums_to_generate / $num_buckets);
-            $max = (int) floor(1.03 * $nums_to_generate / $num_buckets);
+            $min = (int) ceil($minT * $nums_to_generate / $num_buckets);
+            $max = (int) floor($maxT * $nums_to_generate / $num_buckets);
 
             for ($i = 0; $i < $nums_to_generate; ++$i) {
                 $random = random_int(0, 999);
