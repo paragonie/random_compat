3a0c0c47bcd8fa9ffad2bd0c66aef48c70e6c1e8
Scott <scott@paragonie.com>
2015-07-09 17:48:43 -0400

    Add unit tests for updated random_int()

--- tests/unit/RandomIntTest.php
+++ tests/unit/RandomIntTest.php
@@ -12,13 +12,16 @@ class RandomIntTest extends PHPUnit_Framework_TestCase
             random_int(0, 1000),
             random_int(1001,2000),
             random_int(-100, -10),
-            random_int(-1000, 1000)
+            random_int(-1000, 1000),
+            random_int(-PHP_INT_MAX + 1, PHP_INT_MAX)
         );
         
         $this->assertFalse($integers[0] === $integers[1]);
         $this->assertTrue($integers[0] >= 0 && $integers[0] <= 1000);
         $this->assertTrue($integers[1] >= 1001 && $integers[1] <= 2000);
         $this->assertTrue($integers[2] >= -100 && $integers[2] <= -10);
-        $this->assertTrue($integers[2] >= -1000 && $integers[2] <= 1000);
+        $this->assertTrue($integers[3] >= -1000 && $integers[3] <= 1000);
+        $this->assertTrue($integers[3] >= -1000 && $integers[3] <= 1000);
+        $this->assertTrue($integers[4] >= -PHP_INT_MAX + 1 && $integers[4] <= PHP_INT_MAX);
     }
 }
