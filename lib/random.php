4a74efa09be85f8208ea9c10bd541d4d955d80a0
Tom Worster <fsb@thefsb.org>
2015-10-14 15:19:43 -0400

    add OpenSSL warning exception and config const

--- lib/random.php
+++ lib/random.php
@@ -35,6 +35,37 @@ if (PHP_VERSION_ID < 70000) {
     if (!defined('RANDOM_COMPAT_READ_BUFFER')) {
         define('RANDOM_COMPAT_READ_BUFFER', 8);
     }
+    /**
+     * @const RANDOM_COMPAT_ALLOW_OPENSSL Set true to allow use of OpenSSL's random generator
+     */
+    if (!defined('RANDOM_COMPAT_ALLOW_OPENSSL')) {
+        define('RANDOM_COMPAT_ALLOW_OPENSSL', false);
+    }
+    /**
+     * RandomCompatOpenSslWarning is thrown by random_bytes() and random_int() when PHP's only
+     * available source of randomness is OpenSSL and its use is disallowed by configuration.
+     *
+     * It is not always safe to use OpenSSL's random generator. It might not be properly seeded
+     * and is more vulnerable than operating system generators. random_compat is therefore
+     * configured to not use OpenSSL by default.
+     *
+     * To resolve this exception you can
+     * - reconfigure PHP with the Mcrypt extension
+     * - move the application to another PHP environment with Mcrypt or on Linux, BSD or similar OS
+     * - upgrade to PHP 7
+     *
+     * Alternatively you may set RANDOM_COMPAT_ALLOW_OPENSSL true if you are confident OpenSSL's
+     * random generator is safe for your application. In PHP it is not normally possible to know
+     * when OpenSSL's RNG is properly seeded and safe to use in cryptography tasks, such as generating
+     * keys and initialization vectors, or for other things that depend on randomness, such as
+     * one-time passwords, authentication IDs and shared secrets. Since there is no general way
+     * to judge when OpenSSL's generator is tolerable in a given PHP application, if you have any
+     * doubt then the safe choice is to not use it.
+     */
+    class RandomCompatOpenSslWarning extends \Exception {
+        protected $message = 'random_bytes()/random_int() failed to access a reliable source of '
+        . 'randomness and is not allowed to use OpenSSL. See RandomCompatOpenSslWarning documentation.';
+    }
     require_once "byte_safe_strings.php";
     require_once "error_polyfill.php";
     if (!function_exists('random_bytes')) {
@@ -75,6 +106,9 @@ if (PHP_VERSION_ID < 70000) {
             // See random_bytes_com_dotnet.php
             require_once "random_bytes_com_dotnet.php";
         } elseif (extension_loaded('openssl') && PHP_VERSION_ID >= 50300) {
+            if (!RANDOM_COMPAT_ALLOW_OPENSSL) {
+                throw new \RandomCompatOpenSslWarning;
+            }
             // See random_bytes_openssl.php
             require_once "random_bytes_openssl.php";
         } else {
