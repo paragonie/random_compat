736f6b0f70338b48d0cb597f6f9c5272fca254e1
Paragon Initiative Enterprises <security@paragonie.com>
2017-02-03 13:36:43 -0500

    Add missing files.

---
+++ psalm-autoload.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'lib/byte_safe_strings.php';
+require_once 'lib/cast_to_int.php';
+require_once 'lib/error_polyfill.php';
+require_once 'lib/random.php';
+
+$int = random_int(0, 65536);
