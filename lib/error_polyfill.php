26ae9a6b24ca340e8c529a1f5e9183e04c6cd6fc
Chiller Dragon <chillerdragon@gmail.com>
2018-09-06 23:17:43 +0200

    Remove spaces in error_polyfill.php

--- lib/error_polyfill.php
+++ lib/error_polyfill.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Random_* Compatibility Library 
+ * Random_* Compatibility Library
  * for using the new PHP 7 random_* API in PHP 5 projects
- * 
+ *
  * The MIT License (MIT)
  *
  * Copyright (c) 2015 - 2018 Paragon Initiative Enterprises
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
